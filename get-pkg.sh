d5da2e9e61829b0ba1fedc07083e2ff94ac45708
luopengift <870148195@qq.com>
2018-10-13 21:06:50 +0800

    Update get-pkg.sh

--- get-pkg.sh
+++ get-pkg.sh
@@ -4,6 +4,8 @@ if [ "$PKG" == "" ]; then
   echo "Usage: ./get-pkg.sh ctypto"
   exit -1
 fi
+
 set -x
+#git -C $GOPATH/src/github.com/golang/ clone https://github.com/golang/$PKG.git
 go get github.com/golang/$PKG
 ln -s $GOPATH/src/github.com/golang/$PKG $GOPATH/src/golang.org/x/$PKG
